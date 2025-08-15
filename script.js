f01ec2b597b506a015adbc1db7342e2f5f061ae7
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-15 13:26:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+let volumeBtn=document.getElementById("volumebtn");
+let time=0;
+toggle.addEventListener("click",()=>{
+	if(video.paused || video.ended){
+		video.play();
+		toggle.textContent="❚ ❚"
+	}
+	else{
+		video.pause();
+		toggle.textContent="►"
+	}
+})
+
+volumeBtn.addEventListener("input",()=>{
+	video.volume=volumeBtn.value;
+})
+
+video.addEventListener("loadedmetadata",()=>{
+	 time=parseInt(video.duration);
+
+})
+
+video.addEventListener("timeupdate",()=>{
+    progressBar.style.flexBasis=video.currentTime+"px";
+	console.log(video.curre);
+	
+})
